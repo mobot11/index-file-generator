ec6f206e01bc3ff2d9ff9064b3075729e6db435b
Dylan Moberg <dylan.moberg@concur.com>
2019-02-15 20:18:28 -0800

    log args to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello-world');
+#!/usr/bin/env node
+
+import path from 'path';
+import fs from 'fs';
+import chalk from 'chalk';
+import arg from 'arg';
+
+const warning = message => chalk`{yellow WARNING:} ${message}`;
+const info = message => chalk`{magenta INFO:} ${message}`;
+const error = message => chalk`{red ERROR:} ${message}`;
+
+const args = arg({
+    '--help': Boolean,
+    '--entry': String,
+    '--recursive': Boolean,
+    '--ignore': String,
+    '--include': String,
+});
+
+console.log(args);
